4018f18dc7ed1d4b54615aaf06c90c53bced35a6
ElyottoSan <gaius.merlin@gmail.com>
2022-12-09 23:12:44 +0100

    Push des assets

---
+++ Assets/Assets/CameraCircleFade-master/Example/Test.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+	public Easing.Type easing;
+	public float delay;
+
+	void Start () {
+		Camera.main.FadeIn(delay, easing);
+	}
+
+	void Update () {
+	
+	}
+}
